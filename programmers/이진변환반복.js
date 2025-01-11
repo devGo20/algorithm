57d902ec632ba2e1508c335f9e0d17793dfdec6c
devGO <been2zz@naver.com>
2025-01-11 15:02:16 +0900

    이진변환반복 조건문 수정

--- programmers/이진변환반복.js
+++ programmers/이진변환반복.js
@@ -1,13 +1,10 @@
 function solution(s) {
   var answer = [0, 0];
-  while (s) {
+  while (s.length > 1) {
     const zeroCount = (s.match(/0/g) || []).length;
     s = parseInt(s.length - zeroCount).toString(2)
     answer[0] += 1
     answer[1] += zeroCount
-    if (s === '1') {
-      break
-    }
   }
   return answer;
 }
