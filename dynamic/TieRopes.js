9346aca85b3949612ca9b2551796e06d351bd8a4
devGO <been2zz@naver.com>
2025-01-12 16:44:38 +0900

    tieRopes

--- dynamic/TieRopes.js
+++ dynamic/TieRopes.js
@@ -1,17 +1,13 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
 function solution(K, A) {
-  let count = 0;
-  let n = A.length;
-  while (i < n - 1) {
-    let tieSum = A[i] + A[i + 1]
-    if (tieSum >= K) {
-      i += 2
+  let count = 0
+  let tieLength = 0
+
+  for (let i = 0; i < A.length; i++) {
+    tieLength += A[i]
+    if (tieLength >= K) {
       count++
-    } else {
-      i++
+      tieLength = 0
     }
   }
-  return count;
+  return count
 }
