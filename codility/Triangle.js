80f415af543adb994b5b543c3737fd9ab7b82ff9
devGO <been2zz@naver.com>
2025-01-17 15:47:30 +0900

    Triangle 조건 단순화

--- codility/Triangle.js
+++ codility/Triangle.js
@@ -4,7 +4,7 @@
 function solution(A) {
   A = A.sort((a, b) => a - b)
   for (let i = 0; i < A.length - 2; i++) {
-    if (A[i] + A[i + 1] > A[i + 2] && A[i + 1] + A[i + 2] > A[i] && A[i + 2] + A[i] > A[i + 1]) {
+    if (A[i] + A[i + 1] > A[i + 2]) {
       return 1;
     }
   }
