3b162d851ca2e83d9062181002d1215d6ca7a8b2
devGO <been2zz@naver.com>
2025-01-17 15:46:18 +0900

    Triangle

---
+++ codility/Triangle.js
@@ -0,0 +1,15 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+  A = A.sort((a, b) => a - b)
+  for (let i = 0; i < A.length - 2; i++) {
+    if (A[i] + A[i + 1] > A[i + 2] && A[i + 1] + A[i + 2] > A[i] && A[i + 2] + A[i] > A[i + 1]) {
+      return 1;
+    }
+  }
+  return 0
+}
+
+const A = [10, 2, 5, 1, 8, 20]; // true
+console.log(solution(A));
